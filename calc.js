58e3091d6d1f80f4ebaf2fb62a235b79161fc57f
dany-cb <danjes002@gmail.com>
2021-10-28 21:29:21 +0530

    Mobile First Approach and Js addition
    Changed the pixel values to fit Android screens. Added Javascript to make the calculator fully functional. Not responsive yet

---
+++ calc.js
@@ -0,0 +1,85 @@
+"use-strict";
+
+let screen = document.querySelector(".calc-screen");
+let buffer = "0";
+let total = 0;
+let preOp = null;
+let overwrite = true;
+
+document
+	.querySelectorAll(".calc-btn")
+	.forEach((btn) => btn.addEventListener("click", (ev) => buttonPress(ev.target.innerText), false));
+
+document.addEventListener("keydown", (ev) => buttonPress(ev.key), false);
+
+function buttonPress(value) {
+	if (isNaN(value)) {
+		handleSymbol(value);
+	} else {
+		handleNumber(value);
+	}
+	reset();
+}
+
+function reset() {
+	if (buffer < 0) {
+		screen.innerText = buffer.substring(1, buffer.length) + "-";
+	} else {
+		screen.innerText = buffer;
+	}
+}
+
+function handleNumber(value) {
+	if (overwrite) {
+		buffer = value;
+		overwrite = false;
+	} else {
+		buffer += value;
+	}
+}
+
+function handleSymbol(value) {
+	switch (value) {
+		case "Clear":
+			buffer = "0";
+			total = 0;
+			overwrite = true;
+			break;
+
+		case "←":
+			buffer = buffer.substring(0, buffer.length - 1);
+			if (!buffer) {
+				buffer = "0";
+				overwrite = true;
+			}
+			break;
+
+		case "+":
+		case "-":
+		case "*":
+		case "/":
+		case "=":
+			performCalc(value);
+			break;
+	}
+}
+
+function performCalc(value) {
+	if (!overwrite) {
+		if (preOp) {
+			total = Math.round(eval(total + preOp + buffer));
+		} else {
+			total = buffer;
+		}
+
+		buffer = total;
+		overwrite = true;
+	}
+	preOp = value;
+
+	if (value === "=") {
+		overwrite = false;
+		preOp = null;
+		// console.log("Equal");
+	}
+}
